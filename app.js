044f60beb3ea0fcbbaa89ca16bc74796411125ba
aroragirish <aroragirish08@gmail.com>
2022-07-03 14:28:41 +0530

    added changes related to data fetch

--- app.js
+++ app.js
@@ -2,11 +2,55 @@ const express = require('express');
 const mysql = require('mysql');
 var bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const url = require('url');
 
 const app = express();
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const SECRET_KEY = 'testBE'
+const SECRET_KEY = 'testBE';
+
+const jwtTokenValidate = (req, res, next) => {
+    // check header or url parameters or post parameters for token
+    var token = req?.body?.token || req?.query?.token || req?.headers['x-access-token'];
+  
+    // decode token
+    if (token) {
+  
+      // verifies secret and checks exp
+      jwt.verify(token, SECRET_KEY, function(err, decoded) {
+        if (err) {
+            console.log(err);
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;
+          next();
+        }
+      });
+  
+    } else {
+  
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      });
+  
+    }
+  }
+
+app.use((req, res, next) => {
+    const path = url.parse(req.url).pathname;
+    console.log(path);
+  
+    //No JWT token check
+    if (/^\/login/.test(path)) {
+      return next();
+    }
+  
+    return jwtTokenValidate(req, res, next);
+  });
 
 const db = mysql.createConnection({
     host: 'mytestdb.cfmqtzlsywwt.ap-south-1.rds.amazonaws.com',
@@ -22,13 +66,14 @@ db.connect((err) => {
     } else {
         console.log("DB Connected Successfully");
     }
-})
+});
 
 app.post('/login', jsonParser, (req, res) => {
     const body = req.body;
     db.query(`select * from AuthTable where EmailID="${body.userId}" and Password="${body.password}"`, (err, result, field) => {
         if (err) {
             console.log(err);
+            res.status(500).send('Something went wrong');
         }
         if (result.length) {
             const token = jwt.sign({
@@ -44,6 +89,28 @@ app.post('/login', jsonParser, (req, res) => {
             res.status(401).send('Access denied');
         }
     });
+});
+
+app.post('/getData', jsonParser, (req, res) => {
+    const { placeId } = req.body;
+    db.query(`select * from UserTable where PlaceId="${placeId}"`, (err, result, field) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Something went wrong');
+        }
+        if (result.length) {
+            console.log(result);
+            res.status(200).json({
+                data: result[0]
+            })
+        } else {
+            res.status(404).send('Data not found');
+        } 
+    })
+});
+
+app.get('/', (req, res) => {
+    res.send('success')
 });
 
 app.listen(5000, () => {
