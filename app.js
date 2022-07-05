45f6e1433a8d1fbfbad14efbcbbc5d8be6cbf43f
aroragirish <aroragirish08@gmail.com>
2022-07-05 20:35:32 +0530

    added insert service

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const mysql = require('mysql');
-var cors = require('cors')
-var bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const url = require('url');
+const express = require("express");
+const mysql = require("mysql");
+var cors = require("cors")
+var bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const url = require("url");
 
 const app = express();
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const SECRET_KEY = 'testBE';
+const SECRET_KEY = "testBE";
 
 const jwtTokenValidate = (req, res, next) => {
     // check header or url parameters or post parameters for token
-    var token = req?.body?.token || req?.query?.token || req?.headers['x-access-token'];
+    var token = req?.body?.token || req?.query?.token || req?.headers["x-access-token"];
 
     // decode token
     if (token) {
@@ -21,7 +21,7 @@ const jwtTokenValidate = (req, res, next) => {
       jwt.verify(token, SECRET_KEY, function(err, decoded) {
         if (err) {
             console.log(err);
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
+          return res.json({ success: false, message: "Failed to authenticate token." });
         } else {
           // if everything is good, save to request for use in other routes
           req.decoded = decoded;
@@ -35,7 +35,7 @@ const jwtTokenValidate = (req, res, next) => {
       // return an error
       return res.status(403).send({
         success: false,
-        message: 'No token provided.'
+        message: "No token provided."
       });
 
     }
@@ -56,10 +56,10 @@ app.use((req, res, next) => {
   app.use(cors())
 
 const db = mysql.createConnection({
-    host: 'mytestdb.cfmqtzlsywwt.ap-south-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'test1234',
-    database: 'testschema',
+    host: "mytestdb.cfmqtzlsywwt.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "test1234",
+    database: "testschema",
     port: 3306
 });
 
@@ -72,13 +72,13 @@ db.connect((err) => {
 });
 
 app.use(jsonParser);
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
         console.log(req);
     const body = req.body;
     db.query(`select * from AuthTable where EmailID="${body?.userId}" and Password="${body?.password}"`, (err, result, field) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Something went wrong');
+            res.status(500).send("Something went wrong");
         }
         if (result.length) {
             const token = jwt.sign({
@@ -91,17 +91,17 @@ app.post('/login', (req, res) => {
                 token: token
             })
         } else {
-            res.status(401).send('Access denied');
+            res.status(401).send("Access denied");
         }
     });
 });
 
-app.post('/getData', jsonParser, (req, res) => {
+app.post("/getData", jsonParser, (req, res) => {
     const { placeId } = req.body;
     db.query(`select * from UserTable where PlaceId="${placeId}"`, (err, result, field) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Something went wrong');
+            res.status(500).send("Something went wrong");
         }
         if (result.length) {
             console.log(result);
@@ -109,15 +109,31 @@ app.post('/getData', jsonParser, (req, res) => {
                 data: result[0]
             })
         } else {
-            res.status(404).send('Data not found');
+            res.status(404).send("Data not found");
         }
     })
 });
 
-app.get('/', (req, res) => {
-    res.send('success')
+app.post("/postData", jsonParser, (req, res) => {
+    const body = req.body;
+    db.query(`insert into UserTable( address , NoOfIndividuals ,  Above18  ,  Below18 ,  CalenderYear ,  YealyTax ,PlaceId,pendingTax) values ("${body.address}",${body.NoOfIndividuals},${body.Above18}, ${body.Below18},${body.CalenderYear}, ${body.YealyTax},"${body.PlaceId}",${body.pendingTax})`, (err, result, field) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Something went wrong");
+        }
+        if (result) {
+            console.log(result);
+            res.status(200).json({
+                data: result
+            })
+        }
+    })
+});
+
+app.get("/", (req, res) => {
+    res.send("success")
 });
 
 app.listen(5000, () => {
-    console.log('server started on 5k');
+    console.log("server started on 5k");
 })
